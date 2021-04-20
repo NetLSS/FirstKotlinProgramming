2a2de9b5d2600dfb5fe2092310b8347fdc1306a2
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-20 15:56:11 +0900

    Int, Long, 16, 2 진수

---
+++ src/main/kotlin/chap02/section2/DataType_int.kt
@@ -0,0 +1,15 @@
+package chap02.section2
+
+fun main(num07: Int, exp02: Long){
+    val num05 = 127 // Int
+    val num06 = -32768 // Int
+    val num07 = 2147483647 // Int
+    val num08 = 9223372036854775807 // Long
+
+    val exp01 = 123
+    val exp02 = 123L // Long
+    val exp03 = 0x0F // Int 16진수
+    val exp04 = 0b00001011 // Int 2진수
+    // 8진수는 지원하지 않음
+
+}
