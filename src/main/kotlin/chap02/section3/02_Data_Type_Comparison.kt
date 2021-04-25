dc87dc13be01448d516379800a1ee3cb3d1a0651
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-25 22:27:40 +0900

    Feat: 데이터 타입 변환 코드 실습

--- src/main/kotlin/chap02/section3/02_Data_Type_Comparison.kt
+++ src/main/kotlin/chap02/section3/02_Data_Type_Comparison.kt
@@ -9,4 +9,26 @@ fun main() {
     val b: Double = a.toDouble()
 
     val result = 1L + 3 // Long // 표현식 에서는 범위가 큰 자료형으로 자동 형변환
+
+    val c: Byte = 10
+    val d: Short = 55
+    val e: Float = c + d.toFloat()
+    val f: Char = e.toChar()
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+
+    /*
+    1
+    1.0
+    10
+    55
+    65.0
+    A
+
+     */
 }
