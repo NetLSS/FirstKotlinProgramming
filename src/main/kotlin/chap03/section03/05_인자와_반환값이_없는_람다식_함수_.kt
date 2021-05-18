41024624a07024b7d02ad748777dd37ad93dfc1f
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-18 23:47:15 +0900

    ch03-03: 인자와 반환값이 없는 람다식 함수

---
+++ src/main/kotlin/chap03/section03/05_인자와_반환값이_없는_람다식_함수_.kt
@@ -0,0 +1,13 @@
+package chap03.section03
+
+fun main() {
+    val out: () -> Unit = {println("Hello World!")}
+
+    out()
+
+    val new = out
+    new()
+
+//    Hello World!
+//    Hello World!
+}
