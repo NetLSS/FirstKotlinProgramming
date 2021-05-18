9fe1c958fe8cfaac62ef317f26fd33822cd9a81c
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-18 17:58:24 +0900

    Feat: ushr 이해하기

---
+++ src/main/kotlin/chap02/section4/02_ushr_이해하기_.kt
@@ -0,0 +1,16 @@
+package chap02.section4
+
+fun main() {
+    val a = 5
+    val b = -5
+
+    println(a shr 1)
+    println(a ushr 1)
+    println(b shr 1)
+    println(b ushr 1)
+
+//    2
+//    2
+//    -3
+//    2147483645
+}
