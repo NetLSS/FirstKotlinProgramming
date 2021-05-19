dab40d37b1ae7b68730e66ee3fb1956605983efd
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-19 11:13:12 +0900

    ch03-03: Fix: 코드 수정

--- src/main/kotlin/chap03/section03/12_일반매개변수_람다식매개변수_함께사용_.kt
+++ src/main/kotlin/chap03/section03/12_일반매개변수_람다식매개변수_함께사용_.kt
@@ -2,13 +2,17 @@ package chap03.section03
 
 fun main() {
     // 인자와 함께 람다식 사용
-    withArgs("Arg1", "Arg2", {a, b -> "Hello World! $a $b"})
+    withArgs("Arg1", "Arg2", { a, b -> "Hello World! $a $b" })
 
     // "마지막 인자가 람다식"인 경우 소괄호 바깥으로 분리 가능
-    withArgs("Arg1", "Arg2"){
-        a, b -> "Hello World! $a $b"
+    withArgs("Arg1", "Arg2") { a, b ->
+        "Hello World! $a $b"
     }
+}
 
 fun withArgs(a: String, b: String, out: (String, String) -> String) {
     println(out(a, b))
 }
+
+//Hello World! Arg1 Arg2
+//Hello World! Arg1 Arg2
