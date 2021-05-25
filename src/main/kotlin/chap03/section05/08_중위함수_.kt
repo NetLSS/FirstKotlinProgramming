1fd4951889dddd7040e7942bc3838c3a1251dc69
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-05-26 00:19:50 +0900

    ch03-05: 중위 함수 사용하기 (일반 표현법 예시 추가)

--- src/main/kotlin/chap03/section05/08_중위함수_.kt
+++ src/main/kotlin/chap03/section05/08_중위함수_.kt
@@ -1,14 +1,21 @@
 package chap03.section05
 
 fun main() {
+    // 일반 표현법
+    val _multi = 3.multiply(10)
+
+    // 중위 표현법
     val multi = 3 multiply 10 // 30
     println(multi)
 }
 
-infix fun Int.multiply(x:Int):Int{
+// Int 클래스를 확장해서 multiply() 함수를 새로 정의
+infix fun Int.multiply(x:Int):Int{ // infix 로 선언 시 중위 함수
     println("$this multiply $x")
     return this * x
 }
-//
+
+
+//3 multiply 10
 //3 multiply 10
 //30
