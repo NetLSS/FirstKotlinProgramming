3256f06c0a512f759d8f5d30d18489acb347f373
NetLSS <futureprogrammer@naver.com>
2021-06-25 16:02:55 +0900

    ch08-s03- 07_형식_문자_사용하기_.kt

---
+++ src/main/kotlin/chap08/section03/07_형식_문자_사용하기_.kt
@@ -0,0 +1,15 @@
+package chap08.section03
+
+fun main() {
+    val pi = 3.1415926
+    val dec = 10
+    val s = "hello"
+    println("pi = %.2f, %03d, %s".format(pi, dec, s, "d"))
+}
+
+/*
+pi = 3.14, 010, hello
+
+Process finished with exit code 0
+
+ */
